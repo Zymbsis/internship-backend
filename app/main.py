7b0c593c5ce35d4601e5dcb58e32866acb2cfc29
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2026-06-09 11:39:55 +0200

    Decouple domain exceptions from HTTP status codes.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
-from app.exceptions.base import AppError
-from app.exceptions.handlers import app_error_handler
+from app.exceptions.base import AppError, ForbiddenError, NotFoundError
+from app.exceptions.handlers import handle_app_error, handle_forbidden, handle_not_found
 from app.routers.api import api_router
 
 app = FastAPI()
 
 app.include_router(api_router)
-app.add_exception_handler(AppError, app_error_handler)
+app.add_exception_handler(NotFoundError, handle_not_found)
+app.add_exception_handler(ForbiddenError, handle_forbidden)
+app.add_exception_handler(AppError, handle_app_error)
